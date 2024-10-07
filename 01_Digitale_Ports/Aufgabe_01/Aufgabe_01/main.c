4b2d77cc9fa4d6f078adc2b94c4fabdd0a31b287
felixs274 <felix.scholzen27@gmail.com>
2024-10-07 18:37:25 +0200

    Aufgabe 1.1 erstellt

---
+++ 01_Digitale_Ports/Aufgabe_01/Aufgabe_01/main.c
@@ -0,0 +1,18 @@
+/*
+ * Aufgabe_01.c
+ *
+ * Created: 07.10.2024 18:31:22
+ * Author : Felix
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
