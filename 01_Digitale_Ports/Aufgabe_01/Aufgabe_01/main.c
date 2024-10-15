25e77fec557cc1eea1fbba5ec9f02c58d587c5b6
felixs274 <felix.scholzen27@gmail.com>
2024-10-15 15:44:31 +0200

    1.1 Fertig

--- 01_Digitale_Ports/Aufgabe_01/Aufgabe_01/main.c
+++ 01_Digitale_Ports/Aufgabe_01/Aufgabe_01/main.c
@@ -5,14 +5,23 @@
  * Author : Felix
  */ 
 
+#define F_CPU 16000000UL
+
 #include <avr/io.h>
+#include <util/delay.h>
 
 
-int main(void)
-{
-    /* Replace with your application code */
-    while (1) 
-    {
+int main(void){
+	
+	DDRB |= (1<<DDB2) | (1<<DDB1) | (1<<DDB0);
+	PORTB |= (1<<DDB2) | (1<<DDB1) | (1<<DDB0);
+	
+    while (1){
+		
+		for(short counter = 0; counter <= 7; counter++){
+			PORTB = (PORTB & ~(0b00000111)) | counter;
+			_delay_ms(1000);
+		}
+		
     }
 }
-
