6401c24cb57d6e786626d851a416b0c8675c4247
felixs274 <felix.scholzen27@gmail.com>
2024-10-16 12:03:29 +0200

    Datenblatt hinzugefügt

---
+++ 01_Digitale_Ports/Aufgabe_02/Aufgabe_02/main.c
@@ -0,0 +1,18 @@
+/*
+ * Aufgabe_02.c
+ *
+ * Created: 16.10.2024 12:00:52
+ * Author : Felix
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
